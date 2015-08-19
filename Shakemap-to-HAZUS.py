e85273d7e624b6387486bb4aaa8c456f83527123
Josh Groeneveld <joshua.a.groeneveld@gmail.com>
2015-08-19 09:54:58 -0600

    Fixed typo: shake.zip to shape.zip

--- Shakemap-to-HAZUS.py
+++ Shakemap-to-HAZUS.py
@@ -1,4 +1,4 @@
-# The purpose of this script is to take an input shake.zip file from
+# The purpose of this script is to take an input shape.zip file from
 # a USGS ShakeMap and convert the necessary files into a *.mdb file
 # that can be used in HAZUS.
 
@@ -7,7 +7,7 @@
 # Updated On: 08.19.2015
 # Copyright: 2015
 
-"""NOTES: This script assumes that the shake.zip file has been downloaded
+"""NOTES: This script assumes that the shape.zip file has been downloaded
 to a local drive.  While a typical USGS ShakeMap includes a hazus.zip file,
 this file represents average ground motions and not peak ground motions.  For
 our HAZUS analyses, we have been using peak ground motions to model worst-case
@@ -29,7 +29,7 @@ class MainFrame(wx.Frame):
 
         self.mainPanel = wx.Panel(self)
 
-        self.SetTitle("Convert shake.zip to *.mdb for HAZUS")
+        self.SetTitle("Convert shape.zip to *.mdb for HAZUS")
 
         self.sb = self.CreateStatusBar()
         self.sb.SetStatusText("Please select your output directory.")
@@ -43,7 +43,7 @@ class MainFrame(wx.Frame):
                                                 pos=wx.Point(8, 145), size=wx.Size(476, 55))
 
         # the shape.zip box
-        self.serverinfo_staticbox = wx.StaticBox(self.mainPanel, -1, "Select the location of the shake.zip file",
+        self.serverinfo_staticbox = wx.StaticBox(self.mainPanel, -1, "Select the location of the shape.zip file",
                                                 pos=wx.Point(8, 200), size=wx.Size(476, 75))
 
         # the go button box
@@ -52,9 +52,9 @@ class MainFrame(wx.Frame):
 
         # welcome text
         welcomemessage = "The hazus.zip file that comes with a standard USGS ShakeMap product has average ground" \
-                         " motions instead of peak ground motions.  This tool converts the shake.zip file and its" \
+                         " motions instead of peak ground motions.  This tool converts the shape.zip file and its" \
                          " peak ground motions into a *.mdb file that you can then import into HAZUS for your own" \
-                         " HAZUS earthquake scenario.  For more information on the differences between shake.zip" \
+                         " HAZUS earthquake scenario.  For more information on the differences between shape.zip" \
                          " and hazus.zip, follow this link: http://earthquake.usgs.gov/research/shakemap/formats.php"
 
         self.welcome_label = wx.StaticText(self.mainPanel, -1, welcomemessage, pos=wx.Point(20, 30),
@@ -66,13 +66,13 @@ class MainFrame(wx.Frame):
         self.output_directory = ""
         self.output_directory_dialog_button.Bind(wx.EVT_BUTTON, self.select_output_directory)
 
-        # Create a file picker to choose the location of the shake.zip file
+        # Create a file picker to choose the location of the shape.zip file
         self.input_file_button = wx.FilePickerCtrl(self.mainPanel, pos=wx.Point(20, 230), size=wx.Size(450, -1),
                                         wildcard="*.zip")
         self.input_file = ""
         self.input_file_button.Bind(wx.EVT_FILEPICKER_CHANGED, self.select_input_file)
 
-        # Create a button that executes the process to extract the shake.zip file
+        # Create a button that executes the process to extract the shape.zip file
         self.extract_file_button = wx.Button(self.mainPanel, label="Go!", pos=wx.Point(20, 300),
                                         size=wx.Size(150, 60))
         self.Bind(wx.EVT_BUTTON, self.extract_file, self.extract_file_button)
@@ -89,19 +89,19 @@ class MainFrame(wx.Frame):
             self.output_directory = dlg.GetPath()
             self.sb.SetStatusText("You chose %s" % self.output_directory)
         dlg.Destroy()
-        self.sb.SetStatusText("Select the location of your shake.zip file.")
+        self.sb.SetStatusText("Select the location of your shape.zip file.")
 
-    # 3. Select the location of the shake.zip file
+    # 3. Select the location of the shape.zip file
     def select_input_file(self, event):
-        """This function allows the user to choose the location of the shake.zip file"""
+        """This function allows the user to choose the location of the shape.zip file"""
         obj = event.GetEventObject()
         path = event.GetPath()
         self.input_file = path
         self.sb.SetStatusText("You chose %s" % self.input_file)
 
-    # 4. Extract the shake.zip file using the zipfile module
+    # 4. Extract the shape.zip file using the zipfile module
     def extract_file(self, event):
-        """This function takes the user input path to the shake.zip file and
+        """This function takes the user input path to the shape.zip file and
         unzips it in the output directory."""
         self.sb.SetStatusText("This button doesn't do anything...yet")
 
